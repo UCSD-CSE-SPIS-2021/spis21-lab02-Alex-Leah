2cb9f9ff1b6342f52617f6759efed60ffc049fe3
alex hernandez <88351332+alexavndra@users.noreply.github.com>
2021-08-06 21:24:46 +0000

    modified the getNumber() function

--- main.py
+++ main.py
@@ -6,7 +6,27 @@ def sumTwo(a,b):
 
    return c
 
+# modify this function to repeat
 
+def getNumber():
+  
+  finalnumber = " "
+  
+  symbols = input("Enter a digit: ")
+  
+  number = int(symbols)
+  
+  while number != -1:
+    finalnumber = finalnumber + str(number)
+    
+    symbols = input("Enter a digit: ")
+    
+    number = int(symbols)
+  
+  return finalnumber
+
+userNumber = getNumber()
+print(userNumber)
 
 x = sumTwo(3,5)
 
